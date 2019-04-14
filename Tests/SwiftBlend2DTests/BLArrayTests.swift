67009035ef04401405fbe13ecb7eb609679378f7
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-04-14 18:55:30 -0300

    Adding unit tests for some methods of BLArray

---
+++ Tests/SwiftBlend2DTests/BLArrayTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+import blend2d
+@testable import SwiftBlend2D
+
+class BLArrayTests: XCTestCase {
+    func testInit() {
+        let sut = BLArray(type: BL_IMPL_TYPE_ARRAY_U8)
+        
+        XCTAssertEqual(sut.count, 0)
+    }
+    
+    func testAppendU8() {
+        let sut = BLArray(type: BL_IMPL_TYPE_ARRAY_U8)
+        
+        sut.append(0 as UInt8)
+        
+        XCTAssertEqual(sut.count, 1)
+    }
+    
+    func testReserveCapacity() {
+        let sut = BLArray(type: BL_IMPL_TYPE_ARRAY_U8)
+        
+        sut.reserveCapacity(16)
+        
+        XCTAssert(sut.capacity >= 16)
+    }
+    
+    func testShrink() {
+        let sut = BLArray(type: BL_IMPL_TYPE_ARRAY_U8)
+        sut.reserveCapacity(128)
+        sut.append(0 as UInt8)
+        sut.append(1 as UInt8)
+        sut.append(2 as UInt8)
+        
+        sut.shrink()
+        
+        XCTAssertEqual(sut.count, 3)
+        XCTAssert(sut.capacity <= 32)
+    }
+}
