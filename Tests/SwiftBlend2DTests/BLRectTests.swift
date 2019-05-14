3f6582f6b95abc69a1f31f752ae8b6548395f1f4
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-05-14 17:31:55 -0300

    Updating Linux test manifests

--- Tests/SwiftBlend2DTests/BLRectTests.swift
+++ Tests/SwiftBlend2DTests/BLRectTests.swift
@@ -16,4 +16,23 @@ class BLRectTests: XCTestCase {
         XCTAssertNotEqual(BLRect(x: 0, y: 1, w: 2, h: 9),
                           BLRect(x: 0, y: 1, w: 2, h: 3))
     }
+
+    func testInset() {
+        let rect = BLRect(x: 0, y: 0, w: 10, h: 10)
+        let result = rect.insetBy(x: 2, y: 2)
+
+        XCTAssertEqual(result.x, 1)
+        XCTAssertEqual(result.y, 1)
+        XCTAssertEqual(result.w, 8)
+        XCTAssertEqual(result.h, 8)
+    }
+
+    func testQuadrants() {
+        let rect = BLRect(x: 2, y: 2, w: 4, h: 4)
+
+        XCTAssertEqual(rect.topLeft, BLPoint(x: 2, y: 2))
+        XCTAssertEqual(rect.topRight, BLPoint(x: 6, y: 2))
+        XCTAssertEqual(rect.bottomLeft, BLPoint(x: 2, y: 6))
+        XCTAssertEqual(rect.bottomRight, BLPoint(x: 6, y: 6))
+    }
 }
