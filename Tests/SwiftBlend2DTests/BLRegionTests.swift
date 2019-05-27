a985c13d35a7a1cb45489b17b97bc0e7f745aa2d
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-05-27 19:36:07 -0300

    Adding test case for fixed issue on BLRegion

--- Tests/SwiftBlend2DTests/BLRegionTests.swift
+++ Tests/SwiftBlend2DTests/BLRegionTests.swift
@@ -15,4 +15,19 @@ class BLRegionTests: XCTestCase {
             ]
         )
     }
+
+    func testSubtract() {
+        var region = BLRegion()
+        region.combine(box: BLBoxI(x0: 0, y0: 0, x1: 100, y1: 100), operation: .or)
+        region.combine(box: BLBoxI(x0: 25, y0: 25, x1: 75, y1: 75), operation: .sub)
+
+        XCTAssertEqual(
+            region.regionScans, [
+                BLBoxI(x0: 0, y0: 0, x1: 100, y1: 25),
+                BLBoxI(x0: 0, y0: 25, x1: 25, y1: 75),
+                BLBoxI(x0: 75, y0: 25, x1: 100, y1: 75),
+                BLBoxI(x0: 0, y0: 75, x1: 100, y1: 100)
+            ]
+        )
+    }
 }
