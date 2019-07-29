702846ced27097ace66b654a897e95eeac744722
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-07-29 07:40:11 -0300

    Updating Blend2D source code to latest version

--- Sources/blend2d/blend2d/blfontdefs.h
+++ Sources/blend2d/blend2d/blfontdefs.h
@@ -765,7 +765,15 @@ struct BLFontTable {
 
   // --------------------------------------------------------------------------
   #ifdef __cplusplus
-  BL_INLINE void reset() noexcept { memset(this, 0, sizeof(*this)); }
+  BL_INLINE void reset() noexcept {
+    data = nullptr;
+    size = 0;
+  }
+
+  BL_INLINE void reset(const uint8_t* data_, size_t size_) noexcept {
+    data = data_;
+    size = size_;
+  }
 
   template<typename T>
   BL_INLINE const T* dataAs() const noexcept { return reinterpret_cast<const T*>(data); }
@@ -1056,25 +1064,25 @@ struct BLFontDesignMetrics {
 
   union {
     struct {
-      //! Ascent (horizontal).
+      //! Ascent (horizontal layout).
       int ascent;
-      //! Ascent (vertical).
+      //! Ascent (vertical layout).
       int vAscent;
-      //! Descent (horizontal).
+      //! Descent (horizontal layout).
       int descent;
-      //! Descent (vertical).
+      //! Descent (vertical layout).
       int vDescent;
-      //! Minimum leading-side bearing (horizontal).
+      //! Minimum leading-side bearing (horizontal layout).
       int hMinLSB;
-      //! Minimum leading-side bearing (vertical).
+      //! Minimum leading-side bearing (vertical layout).
       int vMinLSB;
-      //! Minimum trailing-side bearing (horizontal).
+      //! Minimum trailing-side bearing (horizontal layout).
       int hMinTSB;
-      //! Minimum trailing-side bearing (vertical).
+      //! Minimum trailing-side bearing (vertical layout).
       int vMinTSB;
-      //! Maximum horizontal advance.
+      //! Maximum advance (horizontal layout).
       int hMaxAdvance;
-      //! Maximum vertical advance.
+      //! Maximum advance (vertical layout).
       int vMaxAdvance;
     };
 
