1e0ba873791e8ca3cf0c2c8b9425c36cb1e24904
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-08-20 05:10:53 -0300

    Marking method as public

--- Sources/SwiftBlend2D/BLPixelConverter.swift
+++ Sources/SwiftBlend2D/BLPixelConverter.swift
@@ -10,13 +10,13 @@ public class BLPixelConverter: BLBaseClass<BLPixelConverterCore> {
     }
     
     @discardableResult
-    func convert(dstData: UnsafeMutableRawPointer,
-                 dstStride: Int,
-                 srcData: UnsafeRawPointer,
-                 srcStride: Int,
-                 width: UInt32,
-                 height: UInt32,
-                 options: BLPixelConverterOptions? = nil) -> BLResult {
+    public func convert(dstData: UnsafeMutableRawPointer,
+                        dstStride: Int,
+                        srcData: UnsafeRawPointer,
+                        srcStride: Int,
+                        width: UInt32,
+                        height: UInt32,
+                        options: BLPixelConverterOptions? = nil) -> BLResult {
         
         return withUnsafeNullablePointer(to: options) { options in
             return blPixelConverterConvert(&object, dstData, dstStride, srcData,
