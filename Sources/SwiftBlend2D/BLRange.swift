895e187082fc80332e0b85f191ce1826dc2d225b
Luiz Fernando Silva <luiz.silva@concrete.com.br>
2019-09-02 15:20:16 -0300

    Adding BLRange.everything definition

--- Sources/SwiftBlend2D/BLRange.swift
+++ Sources/SwiftBlend2D/BLRange.swift
@@ -1,5 +1,11 @@
 import blend2d
 
+public extension BLRange {
+    static var everything: BLRange {
+        return BLRange(start: 0, end: .max)
+    }
+}
+
 extension BLRange: Equatable {
     @inlinable
     public static func ==(lhs: BLRange, rhs: BLRange) -> Bool {
