f8e363a6fc05baed4242abec44a06d6a88f20b7e
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-01-28 23:33:10 -0300

    Adding BLMatrix.mapBox

--- Sources/SwiftBlend2D/BLMatrix2D.swift
+++ Sources/SwiftBlend2D/BLMatrix2D.swift
@@ -443,6 +443,22 @@ public extension BLMatrix2D {
     func mapPolygon(_ polygon: [BLPoint]) -> [BLPoint] {
         return polygon.map(mapPoint)
     }
+    
+    /// Maps the corners of a given box into a newer minimal box capable of
+    /// containing all four mapped points.
+    @inlinable
+    func mapBox(_ box: BLBox) -> BLBox {
+        let points = [
+            box.topLeft,
+            box.topRight,
+            box.bottomLeft,
+            box.bottomRight
+        ]
+
+        let transformed = mapPolygon(points)
+
+        return BLBox(boundsForPoints: transformed)
+    }
 
     /// Maps the corners of a given rectangle into a newer minimal rectangle
     /// capable of containing all four mapped points.
