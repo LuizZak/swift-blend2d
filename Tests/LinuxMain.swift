2936845afa5aec80e042a6f6f9b61aa1c54e7dfc
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-04-05 15:02:49 -0300

    Updating Linux test manifests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,8 @@
 import XCTest
 
-import LibPNGTests
 import SwiftBlend2DTests
 
 var tests = [XCTestCaseEntry]()
-tests += LibPNGTests.__allTests()
 tests += SwiftBlend2DTests.__allTests()
 
 XCTMain(tests)
